d7572cdfdc0c0b80d9adf530aed0b63e71a01653
James Bowers <james@james-bowers.me>
2020-08-04 16:59:14 +0100

    postgres for subscription logic

---
+++ lib/spine/bus_db/postgres.ex
@@ -0,0 +1,68 @@
+defmodule Spine.BusDb.Postgres do
+  alias Spine.BusDb.Postgres.Schema
+
+  def subscribe(repo, channel, starting_event_number) do
+    Schema.Subscription.changeset(%{
+      channel: channel,
+      starting_event_number: starting_event_number,
+      cursor: starting_event_number
+    })
+    |> repo.insert()
+    |> case do
+      {:ok, subscription} -> {:ok, subscription.cursor}
+    end
+  end
+
+  def subscriptions(repo) do
+    repo.all(Schema.Subscription)
+    |> Enum.reduce(%{}, fn subscription, acc ->
+      Map.put(acc, subscription.channel, subscription.cursor)
+    end)
+  end
+
+  def cursor(repo, channel) do
+    repo.get(Schema.Subscription, channel)
+    |> case do
+      %{cursor: cursor} -> cursor
+    end
+  end
+
+  def completed(repo, channel, cursor) do
+    subscription = repo.get!(Schema.Subscription, channel)
+
+    if subscription.cursor == cursor do
+      subscription
+      |> Ecto.Changeset.change(cursor: cursor + 1)
+      |> repo.update()
+      |> case do
+        {:ok, _changes} -> :ok
+      end
+    else
+      :ok
+    end
+  end
+
+  defmacro __using__(repo: repo) do
+    quote do
+      @behaviour Spine.BusDb
+      @repo unquote(repo)
+      alias Spine.BusDb.Postgres
+
+      def subscribe(channel, starting_event_number \\ 0) do
+        Postgres.subscribe(@repo, channel, starting_event_number)
+      end
+
+      def subscriptions do
+        Postgres.subscriptions(@repo)
+      end
+
+      def cursor(channel) do
+        Postgres.cursor(@repo, channel)
+      end
+
+      def completed(channel, cursor) do
+        Postgres.completed(@repo, channel, cursor)
+      end
+    end
+  end
+end
---
+++ test/spine/bus/postgres_test.exs
@@ -0,0 +1,48 @@
+defmodule Spine.BusDb.PostgresTest do
+  use ExUnit.Case
+  use Test.Support.Helper, repo: Test.Support.Repo
+
+  defmodule PostgresTestDb do
+    use Spine.BusDb.Postgres, repo: Test.Support.Repo
+  end
+
+  test "subscribes listener to a channel" do
+    assert {:ok, 0} = PostgresTestDb.subscribe("channel-one")
+  end
+
+  test "retrieves subscriptions" do
+    PostgresTestDb.subscribe("channel-one")
+
+    assert %{
+             "channel-one" => 0
+           } == PostgresTestDb.subscriptions()
+  end
+
+  test "get cursor for the channel" do
+    PostgresTestDb.subscribe("channel-one")
+
+    assert 0 == PostgresTestDb.cursor("channel-one")
+  end
+
+  describe "when an event is completed" do
+    test "increments cursor when :completed message received for correct cursor" do
+      PostgresTestDb.subscribe("channel-one")
+
+      assert :ok == PostgresTestDb.completed("channel-one", 0)
+
+      assert %{
+               "channel-one" => 1
+             } == PostgresTestDb.subscriptions()
+    end
+
+    test "does not increment cursor when :completed message received for incorrect cursor" do
+      PostgresTestDb.subscribe("channel-one")
+
+      assert :ok == PostgresTestDb.completed("channel-one", 1)
+
+      assert %{
+               "channel-one" => 0
+             } == PostgresTestDb.subscriptions()
+    end
+  end
+end
