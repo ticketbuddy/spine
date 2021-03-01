0dfa5eda365b5ce6f5f95c1f5b0a7c5d8d53bc21
James Bowers <james@james-bowers.me>
2021-03-01 19:01:27 +0000

    listener tests

--- test/spine/listener_test.exs
+++ test/spine/listener_test.exs
@@ -53,117 +53,101 @@ defmodule Spine.ListenerTest do
     assert_receive(:subscribe)
   end
 
-  # test "handle_info/1 catches :subscribe message, and starts process work" do
-  #   start_listening_from_event = 5
-  #
-  #   BusDbMock
-  #   |> expect(:subscribe, fn "my-channel", ^start_listening_from_event ->
-  #     {:ok, start_listening_from_event}
-  #   end)
-  #
-  #   CommitNotifier
-  #   |> expect(:subscribe, fn -> :ok end)
-  #
-  #   config = Test.Support.Helper.mock_app_config()
-  #   |> Map.put(:starting_event_number, start_listening_from_event)
-  #
-  #   existing_state = {start_listening_from_event, config}
-  #
-  #   assert {:noreply, {start_listening_from_event, config}} ==
-  #            Spine.Listener.handle_info(:subscribe, existing_state)
-  #
-  #   assert_receive(:process)
-  # end
-  #
-  # describe "handle_info/1 :process message" do
-  #   test "when the event does not exist, do not send :process message" do
-  #     EventStoreMock
-  #     |> expect(:next_event, fn 7 ->
-  #       {:ok, :no_next_event}
-  #     end)
-  #
-  #     config = %{
-  #       channel: "my-channel",
-  #       spine: MyApp,
-  #       callback: ListenerCallbackMock
-  #     }
-  #
-  #     existing_state = {7, config}
-  #
-  #     assert {:noreply, {7, config}} == Spine.Listener.handle_info(:process, existing_state)
-  #
-  #     refute_receive(:process)
-  #   end
-  #
-  #   test "when the next event does exist, and event_handler returns :ok" do
-  #     event = %{an_event: "yes this is"}
-  #
-  #     EventStoreMock
-  #     |> expect(:next_event, fn 7 ->
-  #       # NOTE: as postgres bigserial can have holes,
-  #       # it is possible that event 7 and 8 do not exist.
-  #       # therefore returning 9 is valid
-  #
-  #       {:ok, event, %{event_number: 9, inserted_at: ~U[2020-11-17 19:06:46Z]}}
-  #     end)
-  #
-  #     ListenerCallbackMock
-  #     |> expect(:handle_event, 1, fn ^event, %{channel: "my-channel", cursor: 9} ->
-  #       :ok
-  #     end)
-  #
-  #     BusDbMock
-  #     |> expect(:completed, fn "my-channel", 9 ->
-  #       :ok
-  #     end)
-  #
-  #     config = %{
-  #       channel: "my-channel",
-  #       spine: MyApp,
-  #       callback: ListenerCallbackMock
-  #     }
-  #
-  #     existing_state = {7, config}
-  #
-  #     assert {:noreply, {10, config}} == Spine.Listener.handle_info(:process, existing_state)
-  #
-  #     assert_receive(:process)
-  #   end
-  #
-  #   test "when the next event does exist, and event_handler returns an error" do
-  #     event = %{an_event: "yes this is"}
-  #
-  #     EventStoreMock
-  #     |> expect(:next_event, fn 7 ->
-  #       # NOTE: as postgres bigserial can have holes,
-  #       # it is possible that event 7 and 8 do not exist.
-  #       # therefore returning 9 is valid
-  #       {:ok, event, %{event_number: 9, inserted_at: ~U[2020-11-17 19:06:46Z]}}
-  #     end)
-  #
-  #     ListenerCallbackMock
-  #     |> expect(:handle_event, 1, fn ^event, %{channel: "my-channel", cursor: 9} ->
-  #       {:error, :oh_no}
-  #     end)
-  #
-  #     config = %{
-  #       channel: "my-channel",
-  #       spine: MyApp,
-  #       callback: ListenerCallbackMock
-  #     }
-  #
-  #     existing_state = {7, config}
-  #
-  #     assert {:noreply, {9, config}} == Spine.Listener.handle_info(:process, existing_state)
-  #
-  #     assert_receive(:process)
-  #   end
-  # end
-  #
-  # test "catches stray info messages" do
-  #   message = :anything
-  #   state = :shrug
-  #
-  #   assert {:noreply, state} == Spine.Listener.handle_info(message, state)
-  # end
+  test "handle_info/1 catches :subscribe message, and starts process work", %{
+    listener_config: config
+  } do
+    start_listening_from_event = 5
+    config = Map.put(config, :starting_event_number, start_listening_from_event)
+
+    expect(BusDbMock, :subscribe, fn "mock-channel", ^start_listening_from_event ->
+      {:ok, start_listening_from_event}
+    end)
+
+    expect(CommitNotifierMock, :subscribe, fn -> :ok end)
+
+    existing_state = {start_listening_from_event, config}
+
+    assert {:noreply, {start_listening_from_event, config}} ==
+             Spine.Listener.handle_info(:subscribe, existing_state)
+
+    assert_receive(:process)
+  end
+
+  describe "handle_info/1 :process message" do
+    test "when the event does not exist, do not send :process message", %{listener_config: config} do
+      expect(EventStoreMock, :next_event, fn 7 ->
+        {:ok, :no_next_event}
+      end)
+
+      existing_state = {7, config}
+
+      assert {:noreply, {7, config}} == Spine.Listener.handle_info(:process, existing_state)
+
+      refute_receive(:process)
+    end
+
+    test "when the next event does exist, and event_handler returns :ok", %{
+      listener_config: config
+    } do
+      event = %{an_event: "yes this is"}
+
+      expect(EventStoreMock, :next_event, fn 7 ->
+        # NOTE: as postgres bigserial can have holes,
+        # it is possible that event 7 and 8 do not exist.
+        # therefore returning 9 is valid
+
+        {:ok, event, %{event_number: 9, inserted_at: ~U[2020-11-17 19:06:46Z]}}
+      end)
+
+      expect(ListenerCallbackMock, :handle_event, 1, fn ^event,
+                                                        %{channel: "mock-channel", cursor: 9} ->
+        :ok
+      end)
+
+      expect(BusDbMock, :completed, fn "mock-channel", 9 ->
+        :ok
+      end)
+
+      expect(BusNotifierMock, :broadcast, fn {:listener_completed_event, "mock-channel", 9} ->
+        :ok
+      end)
+
+      existing_state = {7, config}
+
+      assert {:noreply, {10, config}} == Spine.Listener.handle_info(:process, existing_state)
+
+      assert_receive(:process)
+    end
+
+    test "when the next event does exist, and event_handler returns an error", %{
+      listener_config: config
+    } do
+      event = %{an_event: "yes this is"}
+
+      expect(EventStoreMock, :next_event, fn 7 ->
+        # NOTE: as postgres bigserial can have holes,
+        # it is possible that event 7 and 8 do not exist.
+        # therefore returning 9 is valid
+        {:ok, event, %{event_number: 9, inserted_at: ~U[2020-11-17 19:06:46Z]}}
+      end)
+
+      expect(ListenerCallbackMock, :handle_event, 1, fn ^event,
+                                                        %{channel: "mock-channel", cursor: 9} ->
+        {:error, :oh_no}
+      end)
+
+      existing_state = {7, config}
+
+      assert {:noreply, {9, config}} == Spine.Listener.handle_info(:process, existing_state)
+
+      assert_receive(:process)
+    end
+  end
+
+  test "catches stray info messages" do
+    message = :anything
+    state = :shrug
+
+    assert {:noreply, state} == Spine.Listener.handle_info(message, state)
+  end
 end
